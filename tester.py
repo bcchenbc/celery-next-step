b6ee4b903225dc9b4ef728250f55a0ccd3078290
Bo-chiuan Chen <bcchenbc@gmail.com>
2016-07-12 18:07:28 -0700

    correct arguments supplied to add in tester.py

--- tester.py
+++ tester.py
@@ -3,17 +3,16 @@ from proj import tasks
 from random import sample
 
 x, y = sample(range(10), 2)
-a = tasks.add(1,2)
+a = tasks.add(x, y)
 print("{} + {} = {}".format(x,y,a))
 
 x, y = sample(range(10), 2)
-b = tasks.add.apply_async((3,4),)
+b = tasks.add.apply_async((x, y),)
 print("{} + {} = {}".format(x,y,b.get()))
 
 x, y = sample(range(10), 2)
-c = tasks.add.delay(5,6)
+c = tasks.add.delay(x, y)
 print("{} + {} = {}".format(x,y,c.get()))
 
 x, y = sample(range(10), 2)
-d = tasks.add.s(7,8)
-print("{} + {} = {}".format(x,y,d()))
+d = tasks.add.s(x, y)
